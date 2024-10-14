6130569c04791dc3386e609f9d34604a1e859bac
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2024-10-13 23:00:07 -0700

    Update CustomerData.java

--- CustomerData.java
+++ CustomerData.java
@@ -16,42 +16,37 @@
 ////Explanation: Only one customer record in customerData1, none in customerData2.
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CustomerData {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter how many values will be in the 1st array: ");
-        int sizeOfArray1 = scan.nextInt();
+        int m = scan.nextInt();
 
+        ArrayList<Integer> customerData1 = new ArrayList<>();
         System.out.println("Enter data for Customer 1: ");
-        ArrayList<Integer> customerData1 = new ArrayList<>(sizeOfArray1);
-        for (int i = 0; i < sizeOfArray1; i++) {
-            customerData1.add(scan.nextInt()); // stores all input numbers to Array
+        for (int i = 0; i < m; i++) {
+            customerData1.add(scan.nextInt());
         }
 
         System.out.println("Enter how many values will be in the 2nd array: ");
-        int sizeOfArray2 = scan.nextInt();
-        if (sizeOfArray2 > 0) {
+        int n = scan.nextInt();
+        ArrayList<Integer> customerData2 = new ArrayList<>();
+        if (n > 0) {
             System.out.println("Enter data for Customer 2: ");
-        }
-        ArrayList<Integer> customerData2 = new ArrayList<>(sizeOfArray2);
-        for (int n = 0; n < sizeOfArray2; n++) {
-            customerData2.add(scan.nextInt()); // stores all input numbers to Array
-        }
-
-        int mergedArraySize = sizeOfArray1 + sizeOfArray2;
-        ArrayList<Integer> mergedArray = new ArrayList<>(mergedArraySize); // new separate Array for merging 2 arrays
-        for (int i = 0; i < sizeOfArray1; i++) {
-            mergedArray.add(customerData1.get(i)); // should store all values from customerData1 to mergedArray
+            for (int i = 0; i < n; i++) {
+                customerData2.add(scan.nextInt());
+            }
         }
 
-        for (int i = 0; i < sizeOfArray2; i++) {
-            mergedArray.add(customerData2.get(i)); // should store all values from customerData2 to mergedArray
-        }
+        mergeAndSort(customerData1, m, customerData2, n);
+        System.out.println(customerData1);
+    }
 
-        mergedArray.sort(null); // sorting the array
-        System.out.println(mergedArray);
-
+    public static void mergeAndSort(ArrayList<Integer> customerData1, int m, ArrayList<Integer> customerData2, int n) {
+        customerData1.addAll(customerData2);
+        Collections.sort(customerData1);
     }
 }
